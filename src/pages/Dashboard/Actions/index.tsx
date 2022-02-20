f635d0b48c423056c04a2225a100309293fb24dd
icegriffinguru <icegriffin1122@gmail.com>
2022-02-20 16:59:17 +0300

    0.0.1 - Uncaught ReferenceError: process is not defined

--- src/pages/Dashboard/Actions/index.tsx
+++ src/pages/Dashboard/Actions/index.tsx
@@ -1,34 +1,204 @@
 import * as React from 'react';
 import {
+  contractAddress
+} from 'config';
+
+import {
   transactionServices,
   useGetAccountInfo,
   useGetPendingTransactions,
   refreshAccount,
-  useGetNetworkConfig
+  useGetNetworkConfig,
+  DappUI,
+  getAccountProvider
 } from '@elrondnetwork/dapp-core';
 import {
   Address,
   AddressValue,
+  Nonce,
   ContractFunction,
   ProxyProvider,
-  Query
+  WalletProvider,
+  Query,
+  SmartContract,
+  Egld,
+  GasLimit,
+  BigUIntValue,
+  Balance,
+  BytesValue,
+  ArgSerializer,
+  TransactionPayload,
+  AbiRegistry,
+  SmartContractAbi,
+  Interaction,
+  QueryResponseBundle,
+  ReturnCode,
+  TypedValue,
+  U64Value
 } from '@elrondnetwork/erdjs';
+import BigNumber from '@elrondnetwork/erdjs/node_modules/bignumber.js/bignumber.js';
 import { faArrowUp, faArrowDown } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import moment from 'moment';
-import { contractAddress } from 'config';
+
 
 const Actions = () => {
   const account = useGetAccountInfo();
   const { hasPendingTransactions } = useGetPendingTransactions();
   const { network } = useGetNetworkConfig();
   const { address } = account;
+  const proxy = new ProxyProvider(network.apiAddress);
 
   const [secondsLeft, setSecondsLeft] = React.useState<number>();
   const [hasPing, setHasPing] = React.useState<boolean>();
   const /*transactionSessionId*/ [, setTransactionSessionId] = React.useState<
       string | null
     >(null);
+
+    const [tokenId, setTokenId] = React.useState<string | undefined>();
+    const [tokenPrice, setTokenPrice] = React.useState<number | undefined>();
+    const [buyLimit, setBuyLimit] = React.useState<number>();
+    const [startTime, setStartTime] = React.useState<number>();
+    const [endTime, setEndTime] = React.useState<number>();
+    const Millis = 1000;
+  
+    const [contract, setContract] = React.useState<SmartContract>();
+  
+    React.useEffect(() => {
+      
+    }, []); // [] makes useEffect run once
+  
+    const sendQuery = async (interaction: Interaction) => {
+      if (!contract) return;
+      const queryResponse = await contract.runQuery(proxy, interaction.buildQuery());
+      const res = interaction.interpretQueryResponse(queryResponse);
+      return res;
+    };
+  
+    React.useEffect(() => {
+      if (!contract) return;
+      (async () => {
+        const interaction: Interaction = contract.methods.getTokenId();
+        const res: QueryResponseBundle | undefined = await sendQuery(interaction);
+        if (!res || !res.returnCode.isSuccess()) return;
+        const value = res.firstValue.valueOf().toString();
+        setTokenId(value);
+      })();
+    }, [contract]);
+  
+    React.useEffect(() => {
+      if (!contract) return;
+      (async () => {
+        const interaction: Interaction = contract.methods.getTokenPrice();
+        const res: QueryResponseBundle | undefined = await sendQuery(interaction);
+        if (!res || !res.returnCode.isSuccess()) return;
+        const value = parseFloat(Egld.raw(res.firstValue.valueOf()).toDenominated());
+        setTokenPrice(value);
+      })();
+    }, [contract]);
+  
+    React.useEffect(() => {
+      if (!contract) return;
+      (async () => {
+        const interaction: Interaction = contract.methods.getBuyLimit();
+        const res: QueryResponseBundle | undefined = await sendQuery(interaction);
+        if (!res || !res.returnCode.isSuccess()) return;
+        const value = parseFloat(Egld.raw(res.firstValue.valueOf()).toDenominated());
+        setBuyLimit(value);
+      })();
+    }, [contract]);
+  
+    React.useEffect(() => {
+      if (!contract) return;
+      (async () => {
+        const interaction: Interaction = contract.methods.getStartTime();
+        const res: QueryResponseBundle | undefined = await sendQuery(interaction);
+        if (!res || !res.returnCode.isSuccess()) return;
+        const value = parseInt(res.firstValue.valueOf());
+        setStartTime(value);
+      })();
+    }, [contract]);
+    
+    React.useEffect(() => {
+      if (!contract) return;
+      (async () => {
+        const interaction: Interaction = contract.methods.getEndTime();
+        const res: QueryResponseBundle | undefined = await sendQuery(interaction);
+        if (!res || !res.returnCode.isSuccess()) return;
+        const value = parseInt(res.firstValue.valueOf());
+        setEndTime(value);
+      })();
+    }, [contract]);
+  
+    const sendTransaction = async (functionName: string, args: any[]) => {
+      if (!contract) return;
+      const tx = contract.call({
+        func: new ContractFunction(functionName),
+        gasLimit: new GasLimit(5000000),
+        args: args
+      });
+  
+      await refreshAccount();
+  
+      const { sessionId /*, error*/ } = await sendTransactions({
+        transactions: tx,
+        transactionsDisplayInfo: {
+          processingMessage: 'Processing ' + functionName + ' transaction',
+          errorMessage: 'An error has occured during ' + functionName,
+          successMessage: functionName + ' transaction successful'
+        },
+        redirectAfterSign: false
+      });
+    };
+  
+    const updateTokenId = (e: any) => {
+      e.preventDefault();
+      if (!tokenId){
+        alert('Token Id cannot be null.');
+        return;
+      }
+      const args = [BytesValue.fromUTF8(tokenId)];
+      sendTransaction('updateTokenId', args);
+    };
+  
+    const updateTokenPrice = (e: any) => {
+      e.preventDefault();
+      console.log('tokenPrice', tokenPrice);
+      if (!tokenPrice){
+        alert('Token Price cannot be null.');
+        return;
+      }
+      const args = [new BigUIntValue(Balance.egld(tokenPrice).valueOf())];
+      sendTransaction('updateTokenPrice', args);
+    };
+  
+    const updateBuyLimit = (e: any) => {
+      e.preventDefault();
+      if (!buyLimit){
+        alert('Buy Limit cannot be null.');
+        return;
+      }
+      const args = [new BigUIntValue(Balance.egld(buyLimit).valueOf())];
+      sendTransaction('updateBuyLimit', args);
+    };
+  
+    const updateTimes = (e: any) => {
+      e.preventDefault();
+      if (!startTime || !endTime){
+        alert('Start Time and End Time should be set.');
+        return;
+      }
+      if (startTime <= (new Date()).getTime() / Millis){
+        alert('Start Time cannot be past.');
+        return;
+      }
+      if (startTime >= endTime){
+        alert('Start Time cannot be before than End Time.');
+        return;
+      }
+      const args = [new U64Value(new BigNumber(startTime)), new U64Value(new BigNumber(endTime))];
+      sendTransaction('updateTimes', args);
+    };
 
   const mount = () => {
     if (secondsLeft) {
